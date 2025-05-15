0920baa883b888eb49b16656dd2288ba0c3377e2
Mark Thomas <m.marcantano@gmail.com>
2025-05-15 16:53:54 -0700

    add favico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* You might also want to add a theme-color meta tag if your manifest specifies one */}
+        {/* e.g., <meta name="theme-color" content="#ffffff" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
